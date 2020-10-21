a98999a059d10197ce7049f2aaf6cb2731087116
CS19B010Gowtami <70938268+CS19B010Gowtami@users.noreply.github.com>
2020-10-21 20:33:04 +0530

    Update Entrance.java

--- Entrance.java
+++ Entrance.java
@@ -1,6 +1,18 @@
-public class Entrance{
-    String vehicleType;
+public class Entrance
+{
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter vehicle type")
+    String vehicleType=sc.next();
     long entryTime = System.currentTimeMillis();
     //assign spot based on the first available spot
     //print pricing
+    Displayboard.displayboard();
+    String regno=assaignregNo(vehicleType);
+    public static String assaignregNo(String s)
+    {
+        static char i=0;
+        String str=s.concat('i');
+        i++;
+    }
+    assaignspot(regno);
 }
