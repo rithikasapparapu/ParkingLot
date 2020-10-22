7cea377c081653f8276fab70b9e84da84ce4859a
CS19B010Gowtami <70938268+CS19B010Gowtami@users.noreply.github.com>
2020-10-22 12:24:04 +0530

    Update ParkingFloor.java

--- ParkingFloor.java
+++ ParkingFloor.java
@@ -1,6 +1,18 @@
-public class ParkingFloor {
+public class ParkingFloor 
+{
     //Array of parking spots initialized with types of vehicle capacity in each floor
-    //Eg, ParkingSpots[] cars = new ParkingSpots[50];
+    //ParkingSpots[] cars = new ParkingSpots[50];
     //Display board - shows the number of vacancies
     int floorNumber;
+    if(floorNumber==1)
+    {
+        ParkingSpots cars[]= new ParkingSpots[50];
+        ParkingSpots trucks[]=new ParkingSpots[30];
+        ParkingSpots handicapped[]=new ParkingSpots[10];
+        ParkingSpots electriccars[]=new ParkingSpots[10];
+    }
+    if(floorNumber==2||floorNumber==3||floorNumber==4||floorNumber==5)
+    {
+       ParkingSpots 
+    }
 }
